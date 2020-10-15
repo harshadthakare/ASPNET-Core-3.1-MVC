55f1133c95febe34bea64b0eeb9fc65ee0bf3fdd
harshad thakare <harshadt180@gmail.com>
2020-10-15 12:30:36 +0530

    Added static files and accessing in browser 15-10-20

--- MyBookStoreApp/MyBookStoreApp/Startup.cs
+++ MyBookStoreApp/MyBookStoreApp/Startup.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace MyBookStoreApp
@@ -28,6 +30,11 @@ namespace MyBookStoreApp
             }
 
             app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions() {
+               FileProvider=new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),"MystaticFilesTest")),
+               RequestPath= "/MystaticFilesTest"
+            });
 
             app.UseRouting();
 
